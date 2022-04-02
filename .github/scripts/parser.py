bf21fbc126a19e034d30a207fdb6c31484eff507
sigmanificient <sigmanificient@gmail.com>
2022-04-02 19:58:09 +0200

    :bug: Fixing pased value

--- .github/scripts/parser.py
+++ .github/scripts/parser.py
@@ -2,13 +2,17 @@ import re
 
 
 def main():
-    with open('../../test_log.txt') as f:
+    with open('test_log.txt') as f:
         lines = list(filter(bool, f.read().splitlines()))
 
     passed = re.findall(r'^(\d+)%', lines[-2])[0]
+    print(f'{passed}% passed')
 
-    if passed != 100:
+    if passed != '100':
+        print('Test failed')
         exit(1)
+
+    print('All tests passed!')
 
 
 if __name__ == '__main__':
