20ca017c2052a3798e204623c7db7682359719f4
jaggernaute <lucas.videcoq1@gmail.com>
2022-06-09 14:11:45 +0200

    seeder need additional testing

--- utils/DB-seeder/seeder.cpp
+++ utils/DB-seeder/seeder.cpp
@@ -2,30 +2,21 @@
 // Created by jaggernaute on 6/7/22.
 //
 
-#include <map>
-#include <string>
-#include <random>
+#include "../../include/SQL_API.hpp"
+#include <QSqlQuery>
+#include <QDebug>
 
-struct Fake_user {
-        std::string email;
-        std::string first_name;
-        std::string last_name;
-        std::string mail;
-        std::string idBadge;
-        std::string password;
-        std::string last_res;
-};
+auto seed() -> void {
+    SQL_API::instance();
+    QSqlQuery query;
+    QString query_string;
 
-std::string first_name[33] { "Adam", "Alex", "Aaron", "Ben", "Carl", "Dan", "David", "Edward", "Fred", "Frank", "George", "Hal", "Hank", "Ike", "John", "Jack", "Joe", "Larry", "Monte", "Matthew", "Mark", "Nathan", "Otto", "Paul", "Peter", "Roger", "Roger", "Steve", "Thomas", "Tim", "Ty", "Victor", "Walter"};
-
-std::string name[150] { "Anderson", "Ashwoon", "Aikin", "Bateman", "Bongard", "Bowers", "Boyd", "Cannon", "Cast", "Deitz", "Dewalt", "Ebner", "Frick", "Hancock", "Haworth", "Hesch", "Hoffman", "Kassing", "Knutson", "Lawless", "Lawicki", "Mccord", "McCormack", "Miller", "Myers", "Nugent", "Ortiz", "Orwig", "Ory", "Paiser", "Pak", "Pettigrew", "Quinn", "Quizoz", "Ramachandran", "Resnick", "Sagar", "Schickowski", "Schiebel", "Sellon", "Severson", "Shaffer", "Solberg", "Soloman", "Sonderling", "Soukup", "Soulis", "Stahl", "Sweeney", "Tandy", "Trebil", "Trusela", "Trussel", "Turco", "Uddin", "Uflan", "Ulrich", "Upson", "Vader", "Vail", "Valente", "Van Zandt", "Vanderpoel", "Ventotla", "Vogal", "Wagle", "Wagner", "Wakefield", "Weinstein", "Weiss", "Woo", "Yang", "Yates", "Yocum", "Zeaser", "Zeller", "Ziegler", "Bauer", "Baxster", "Casal", "Cataldi", "Caswell", "Celedon", "Chambers", "Chapman", "Christensen", "Darnell", "Davidson", "Davis", "DeLorenzo", "Dinkins", "Doran", "Dugelman", "Dugan", "Duffman", "Eastman", "Ferro", "Ferry", "Fletcher", "Fietzer", "Hylan", "Hydinger", "Illingsworth", "Ingram", "Irwin", "Jagtap", "Jenson", "Johnson", "Johnsen", "Jones", "Jurgenson", "Kalleg", "Kaskel", "Keller", "Leisinger", "LePage", "Lewis", "Linde", "Lulloff", "Maki", "Martin", "McGinnis", "Mills", "Moody", "Moore", "Napier", "Nelson", "Norquist", "Nuttle", "Olson", "Ostrander", "Reamer", "Reardon", "Reyes", "Rice", "Ripka", "Roberts", "Rogers", "Root", "Sandstrom", "Sawyer", "Schlicht", "Schmitt", "Schwager", "Schutz", "Schuster", "Tapia", "Thompson", "Tiernan", "Tisler" };
-
-auto faker() -> void {
-    for ( int i = 0; i < 33; i++ ) {
-        auto random_engine = std::random_device();
-        auto seed = random_engine();
-        std::mt19937 gen(seed);
-        auto dist = std::uniform_int_distribution<>(0, 32);
-        auto dist2 = std::uniform_int_distribution<>(0, 149);
+    QFile file(":/seed.sql");
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        qDebug() << "Error opening file";
     }
+
+    query_string = file.readAll();
+    query.prepare(query_string);
+    query.exec();
 }
